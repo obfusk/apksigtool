e4c8da818d32cb864b6337b1125cee9dd85d3ae5
Felix C. Stegerman <flx@obfusk.net>
2021-08-29 20:55:29 +0200

    CI: skip negmod apk

--- test/test-verify.sh
+++ test/test-verify.sh
@@ -3,6 +3,7 @@ set -e
 export LC_ALL=C.UTF-8
 for apk in apks/*.apk; do
   [[ "$apk" != *empty* ]] || continue
+  [[ "$apk" != *negmod* ]] || continue
   [[ "$apk" != *v1-only* ]] || continue
   echo "$apk"
   if apksigner verify --min-sdk-version=28 "$apk" >/dev/null 2>&1; then
